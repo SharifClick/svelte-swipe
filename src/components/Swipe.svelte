96627d873f4ca028fd6c3f914a0acf49eb49ceeb
Sharif Ahmed <me.sharifahmed@gmail.com>
2019-08-26 00:22:20 +0600

    more styles

--- src/components/Swipe.svelte
+++ src/components/Swipe.svelte
@@ -14,6 +14,20 @@
   position: relative;
   height: calc(45vh - var(--header-height));
 }
+.swipeable-item {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  transition-timing-function: ease-out;
+}
+.swipeable-items,
+.swipeable-slot-wrapper {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
 </style>
 
 <div class="swipe-panel">
