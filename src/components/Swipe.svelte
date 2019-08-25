b68b07ae5ab039c7048c574fbe59e350fed42ff8
Sharif Ahmed <me.sharifahmed@gmail.com>
2019-08-26 00:20:32 +0600

    swipe boiler plate

---
+++ src/components/Swipe.svelte
@@ -0,0 +1,29 @@
+<script>
+
+</script>
+
+<style>
+:root{
+  --header-height: 50px;
+}
+.swipe-panel {
+  position: relative;
+}
+.swipe-item-wrapper{
+  overflow: hidden;
+  position: relative;
+  height: calc(45vh - var(--header-height));
+}
+</style>
+
+<div class="swipe-panel">
+  <div class="swipe-item-wrapper">
+    <div class="swipeable-items">
+      <div class="swipeable-slot-wrapper">
+        <slot />
+      </div>
+    </div>
+  </div>
+  <div class="swipe-handler"></div>
+
+</div>
