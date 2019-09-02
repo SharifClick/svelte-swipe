f14a51aebfc1f0efbed326443a9ebb5dd4ee6bb0
Sharif Ahmed <me.sharifahmed@gmail.com>
2019-09-03 00:36:40 +0600

    temporary class

--- src/App.svelte
+++ src/App.svelte
@@ -3,15 +3,19 @@
 </script>
 
 <style>
-
+  .is-center{
+    display: flex;
+    justify-content: center;
+    border: 1px solid green;
+  }
 </style>
 
 <Swipe>
-  <div class="swipeable-item">
+  <div class="swipeable-item is-center">
     <h1>Boo yeaah</h1>
   </div>
 
-  <div class="swipeable-item">
+  <div class="swipeable-item is-center">
     <h1>Nope :(</h1>
   </div>
 </Swipe>
