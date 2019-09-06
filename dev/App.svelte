e19bd2cf4666ddedfd9af27b3048f8dab3b8416b
Sharif Ahmed <me.sharifahmed@gmail.com>
2019-09-06 18:51:16 +0600

    dev environment

---
+++ dev/App.svelte
@@ -0,0 +1,47 @@
+<script>
+  import Swipe from "../src/Swipe.svelte";
+  let play_slider = false;
+  let play_delay = 1000;
+  
+  function toggle(){
+    play_slider = !play_slider;
+  }
+
+</script>
+
+<style>
+  :root{
+    --sv-swipe-panel-height: 100px;
+    /* --sv-swipe-indicator-active-color:grey; */
+  }
+  .is-center{
+    display: flex;
+    justify-content: center;
+    border: 1px solid green;
+    position: absolute;
+    width: 99%;
+  }
+</style>
+
+<Swipe showIndicators={true} autoplay={play_slider} delay={play_delay}>
+  <div class="swipeable-item is-center">
+    <h1>Boo yeaah</h1>
+  </div>
+
+  <div class="swipeable-item is-center">
+    <h1>Nope :(</h1>
+  </div>
+
+  <div class="swipeable-item is-center">
+    <h1>Oops!</h1>
+  </div>
+
+  <div class="swipeable-item is-center">
+    <h1>Ouch!!!</h1>
+  </div>
+</Swipe>
+
+<div style="margin-top: 100px">
+  <button on:click={toggle} >{play_slider ? 'Stop': 'Play'}</button>
+  <input type="text" bind:value={play_delay} />
+</div>
