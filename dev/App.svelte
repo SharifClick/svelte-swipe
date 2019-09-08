6be374a6bed279fcfaaacab5299e4949baa77264
Sharif Ahmed <me.sharifahmed@gmail.com>
2019-09-08 23:03:56 +0600

    testing autoplay

--- dev/App.svelte
+++ dev/App.svelte
@@ -33,8 +33,8 @@
   }
 </style>
 
-<div class="container">
-  <Swipe showIndicators={true}>
+<div class="container" >
+  <Swipe showIndicators={true} autoplay={play_slider} delay={play_delay}>
     <div class="swipeable-item is-center">
       <img src="./images/1.jpg" alt="">
     </div>
