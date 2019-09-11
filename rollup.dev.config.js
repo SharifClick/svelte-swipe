15b980f6761e1870001b5d4d1d328034e388a760
Sharif Ahmed <me.sharifahmed@gmail.com>
2019-09-11 20:58:34 +0600

    changed to docs

--- rollup.dev.config.js
+++ rollup.dev.config.js
@@ -12,7 +12,7 @@ export default {
 		sourcemap: true,
 		format: 'iife',
 		name: 'app',
-		file: 'public/bundle.js'
+		file: 'docs/bundle.js'
 	},
 	plugins: [
 		svelte({
@@ -21,7 +21,7 @@ export default {
 			// we'll extract any component CSS out into
 			// a separate file — better for performance
 			css: css => {
-				css.write('public/bundle.css');
+				css.write('docs/bundle.css');
 			}
 		}),
 
@@ -36,9 +36,9 @@ export default {
 		}),
 		commonjs(),
 
-		// Watch the `public` directory and refresh the
+		// Watch the `docs` directory and refresh the
 		// browser on changes when not in production
-		!production && livereload('public'),
+		!production && livereload('docs'),
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
