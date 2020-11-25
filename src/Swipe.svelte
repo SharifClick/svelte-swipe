2ed18de2717ed4047ee601617f2b46a3467c23ac
Prarabdh Bharti <prarabdh96@gmail.com>
2020-11-26 02:16:45 +0530

    Fixed import

--- src/Swipe.svelte
+++ src/Swipe.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { onMount, onDestroy } from "svelte";
+	import { onMount, onDestroy, createEventDispatcher } from "svelte";
 
 	export let transitionDuration = 200;
 	export let showIndicators = false;
