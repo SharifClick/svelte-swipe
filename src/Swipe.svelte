e9f53b6db7e29b59a55fa7d0a29943e0850a7d0f
Saket Diwakar <diwakar.saket1994@gmail.com>
2020-11-28 00:36:49 +0530

    Prevent pull to refresh on touch move and touch end

--- src/Swipe.svelte
+++ src/Swipe.svelte
@@ -1,289 +1,293 @@
 <script>
-	import { onMount, onDestroy, createEventDispatcher } from "svelte";
+  import { onMount, onDestroy, createEventDispatcher } from "svelte";
 
-	export let transitionDuration = 200;
-	export let showIndicators = false;
-	export let autoplay = false;
-	export let delay = 1000;
-	export let defaultIndex = 0;
+  export let transitionDuration = 200;
+  export let showIndicators = false;
+  export let autoplay = false;
+  export let delay = 1000;
+  export let defaultIndex = 0;
 
-	export let active_item = 0; //readonly
-	export let is_vertical = false;
-	export let totalItems;
+  export let active_item = 0; //readonly
+  export let is_vertical = false;
+  export let totalItems;
 
-	let activeIndicator = 0,
-		indicators,
-		total_elements = 0,
-		availableSpace = 0,
-		availableWidth = 0,
-		swipeElements,
-		availableDistance = 0,
-		swipeWrapper,
-		pos_axis = 0,
-		page_axis = is_vertical ? "pageY" : "pageX",
-		axis,
-		longTouch,
-		last_axis_pos,
-		isMounted = false;
+  let activeIndicator = 0,
+    indicators,
+    total_elements = 0,
+    availableSpace = 0,
+    availableWidth = 0,
+    swipeElements,
+    availableDistance = 0,
+    swipeWrapper,
+    pos_axis = 0,
+    page_axis = is_vertical ? "pageY" : "pageX",
+    axis,
+    longTouch,
+    last_axis_pos,
+    isMounted = false;
 
-	let played = defaultIndex || 0;
-	let run_interval = false;
+  let played = defaultIndex || 0;
+  let run_interval = false;
 
-	const dispatch = createEventDispatcher();
+  const dispatch = createEventDispatcher();
+  const noSupportPassive = { passive: false };
 
-	function init() {
-		swipeElements = swipeWrapper.querySelectorAll(".swipeable-item");
-		total_elements = swipeElements.length;
-		update();
-	}
+  function init() {
+    swipeElements = swipeWrapper.querySelectorAll(".swipeable-item");
+    total_elements = swipeElements.length;
+    update();
+  }
 
-	$: {
-		if (totalItems && isMounted) {
-			setTimeout(() => {
-				init();
-			}, 500);
-		}
-	}
+  $: {
+    if (totalItems && isMounted) {
+      setTimeout(() => {
+        init();
+      }, 500);
+    }
+  }
 
-	function update() {
-		let { offsetWidth, offsetHeight } = swipeWrapper.querySelector(
-			".swipeable-total_elements"
-		);
-		availableSpace = is_vertical ? offsetHeight : offsetWidth;
-		[...swipeElements].forEach((element, i) => {
-			element.style.transform = generateTranslateValue(
-				availableSpace * i
-			);
-			element.style.zIndex = 1;
-			element.style.opacity = 1;
-		});
-		availableDistance = 0;
-		availableWidth = availableSpace * (total_elements - 1);
-		if (defaultIndex) {
-			changeItem(defaultIndex);
-		}
-	}
+  function update() {
+    let { offsetWidth, offsetHeight } = swipeWrapper.querySelector(
+      ".swipeable-total_elements"
+    );
+    availableSpace = is_vertical ? offsetHeight : offsetWidth;
+    [...swipeElements].forEach((element, i) => {
+      element.style.transform = generateTranslateValue(availableSpace * i);
+      element.style.zIndex = 1;
+      element.style.opacity = 1;
+    });
+    availableDistance = 0;
+    availableWidth = availableSpace * (total_elements - 1);
+    if (defaultIndex) {
+      changeItem(defaultIndex);
+    }
+  }
 
-	$: indicators = Array(total_elements);
+  $: indicators = Array(total_elements);
 
-	$: {
-		if (autoplay && !run_interval) {
-			run_interval = setInterval(changeView, delay);
-		}
+  $: {
+    if (autoplay && !run_interval) {
+      run_interval = setInterval(changeView, delay);
+    }
 
-		if (!autoplay && run_interval) {
-			clearInterval(run_interval);
-			run_interval = false;
-		}
-	}
+    if (!autoplay && run_interval) {
+      clearInterval(run_interval);
+      run_interval = false;
+    }
+  }
 
-	// helpers
-	function eventDelegate(type) {
-		let delegationTypes = {
-			add: "addEventListener",
-			remove: "removeEventListener"
-		};
-		if (typeof window !== "undefined") {
-			window[delegationTypes[type]]("mousemove", onMove);
-			window[delegationTypes[type]]("mouseup", onEnd);
-			window[delegationTypes[type]]("touchmove", onMove);
-			window[delegationTypes[type]]("touchend", onEnd);
-		}
-	}
+  // helpers
+  function eventDelegate(type) {
+    let delegationTypes = {
+      add: "addEventListener",
+      remove: "removeEventListener",
+    };
+    if (typeof window !== "undefined") {
+      window[delegationTypes[type]]("mousemove", onMove, noSupportPassive);
+      window[delegationTypes[type]]("mouseup", onEnd, noSupportPassive);
+      window[delegationTypes[type]]("touchmove", onMove, noSupportPassive);
+      window[delegationTypes[type]]("touchend", onEnd, noSupportPassive);
+    }
+  }
 
-	function normalizeEventBehavior(e) {
-		e && e.stopImmediatePropagation();
-		e && e.stopPropagation();
-	}
+  function normalizeEventBehavior(e) {
+    e && e.stopImmediatePropagation();
+    e && e.stopPropagation();
+  }
 
-	function generateTranslateValue(value) {
-		return is_vertical
-			? `translate3d(0, ${value}px, 0)`
-			: `translate3d(${value}px, 0, 0)`;
-	}
+  function generateTranslateValue(value) {
+    return is_vertical
+      ? `translate3d(0, ${value}px, 0)`
+      : `translate3d(${value}px, 0, 0)`;
+  }
 
-	function generateTouchPosCss(value, touch_end = false) {
-		let transformString = generateTranslateValue(value);
-		let _css = `
+  function generateTouchPosCss(value, touch_end = false) {
+    let transformString = generateTranslateValue(value);
+    let _css = `
       -webkit-transition-duration: ${touch_end ? transitionDuration : "0"}ms;
       transition-duration: ${touch_end ? transitionDuration : "0"}ms;
       -webkit-transform: ${transformString};
       -ms-transform: ${transformString};
 	  z-index: 1;
 	  opacity: 1;`;
-		return _css;
-	}
+    return _css;
+  }
 
-	onMount(() => {
-		init();
-		isMounted = true;
-		if (typeof window !== "undefined") {
-			window.addEventListener("resize", update);
-		}
-	});
+  onMount(() => {
+    init();
+    isMounted = true;
+    if (typeof window !== "undefined") {
+      window.addEventListener("resize", update);
+    }
+  });
 
-	onDestroy(() => {
-		if (typeof window !== "undefined") {
-			window.removeEventListener("resize", update);
-		}
-	});
+  onDestroy(() => {
+    if (typeof window !== "undefined") {
+      window.removeEventListener("resize", update);
+    }
+  });
 
-	let touch_active = false;
+  let touch_active = false;
 
-	function onMove(e) {
-		if (touch_active) {
-			normalizeEventBehavior(e);
-			let _axis = e.touches ? e.touches[0][page_axis] : e[page_axis],
-				distance = axis - _axis + pos_axis;
-			if (distance <= availableWidth && distance >= 0) {
-				[...swipeElements].forEach((element, i) => {
-					element.style.cssText = generateTouchPosCss(
-						availableSpace * i - distance
-					);
-				});
-				availableDistance = distance;
-				last_axis_pos = _axis;
-			}
-		}
-	}
+  function onMove(e) {
+    if (touch_active) {
+      normalizeEventBehavior(e);
+      let _axis = e.touches ? e.touches[0][page_axis] : e[page_axis],
+        distance = axis - _axis + pos_axis;
+      if (distance <= availableWidth && distance >= 0) {
+        [...swipeElements].forEach((element, i) => {
+          element.style.cssText = generateTouchPosCss(
+            availableSpace * i - distance
+          );
+        });
+        availableDistance = distance;
+        last_axis_pos = _axis;
 
-	function onMoveStart(e) {
-		normalizeEventBehavior(e);
-		touch_active = true;
-		longTouch = false;
-		setTimeout(function () {
-			longTouch = true;
-		}, 250);
-		axis = e.touches ? e.touches[0][page_axis] : e[page_axis];
-		eventDelegate("add");
-	}
+        let direction = axis < last_axis_pos;
+        if (direction) {
+          e && e.preventDefault();
+        }
+      }
+    }
+  }
 
-	function onEnd(e) {
-		normalizeEventBehavior(e);
-		let direction = axis < last_axis_pos;
-		touch_active = false;
-		let _as = availableSpace;
-		let accidental_touch =
-			Math.round(availableSpace / 50) > Math.abs(axis - last_axis_pos);
-		if (longTouch || accidental_touch) {
-			availableDistance = Math.round(availableDistance / _as) * _as;
-		} else {
-			availableDistance = direction
-				? Math.floor(availableDistance / _as) * _as
-				: Math.ceil(availableDistance / _as) * _as;
-		}
+  function onMoveStart(e) {
+    normalizeEventBehavior(e);
+    touch_active = true;
+    longTouch = false;
+    setTimeout(function () {
+      longTouch = true;
+    }, 250);
+    axis = e.touches ? e.touches[0][page_axis] : e[page_axis];
+    eventDelegate("add");
+  }
 
-		axis = null;
-		last_axis_pos = null;
-		pos_axis = availableDistance;
-		activeIndicator = availableDistance / _as;
+  function onEnd(e) {
+    normalizeEventBehavior(e);
+    let direction = axis < last_axis_pos;
+    if (direction) {
+      e && e.preventDefault();
+    }
+    touch_active = false;
+    let _as = availableSpace;
+    let accidental_touch =
+      Math.round(availableSpace / 50) > Math.abs(axis - last_axis_pos);
+    if (longTouch || accidental_touch) {
+      availableDistance = Math.round(availableDistance / _as) * _as;
+    } else {
+      availableDistance = direction
+        ? Math.floor(availableDistance / _as) * _as
+        : Math.ceil(availableDistance / _as) * _as;
+    }
 
-		[...swipeElements].forEach((element, i) => {
-			element.style.cssText = generateTouchPosCss(
-				_as * i - pos_axis,
-				true
-			);
-		});
+    axis = null;
+    last_axis_pos = null;
+    pos_axis = availableDistance;
+    activeIndicator = availableDistance / _as;
 
-		active_item = activeIndicator;
-		defaultIndex = active_item;
-		eventDelegate("remove");
-		dispatch("swipe", {
-			activeIndex: active_item
-		});
-	}
+    [...swipeElements].forEach((element, i) => {
+      element.style.cssText = generateTouchPosCss(_as * i - pos_axis, true);
+    });
 
-	function changeItem(item) {
-		let max = availableSpace;
-		availableDistance = max * item;
-		activeIndicator = item;
-		onEnd();
-	}
+    active_item = activeIndicator;
+    defaultIndex = active_item;
+    eventDelegate("remove");
+    dispatch("swipe", {
+      activeIndex: active_item,
+    });
+  }
 
-	function changeView() {
-		changeItem(played);
-		played = played < total_elements - 1 ? ++played : 0;
-	}
+  function changeItem(item) {
+    let max = availableSpace;
+    availableDistance = max * item;
+    activeIndicator = item;
+    onEnd();
+  }
 
-	export function goTo(step) {
-		let item = Math.max(0, Math.min(step, indicators.length - 1));
-		changeItem(item);
-	}
-	export function prevItem() {
-		let step = activeIndicator - 1;
-		goTo(step);
-	}
+  function changeView() {
+    changeItem(played);
+    played = played < total_elements - 1 ? ++played : 0;
+  }
 
-	export function nextItem() {
-		let step = activeIndicator + 1;
-		goTo(step);
-	}
+  export function goTo(step) {
+    let item = Math.max(0, Math.min(step, indicators.length - 1));
+    changeItem(item);
+  }
+  export function prevItem() {
+    let step = activeIndicator - 1;
+    goTo(step);
+  }
+
+  export function nextItem() {
+    let step = activeIndicator + 1;
+    goTo(step);
+  }
 </script>
 
 <style>
-	.swipe-panel {
-		position: relative;
-		height: var(--sv-swipe-panel-height, 100%);
-		width: var(--sv-swipe-panel-width, inherit);
-	}
-	.swipe-item-wrapper {
-		overflow: hidden;
-		position: relative;
-		height: inherit;
-		z-index: var(--sv-swipe-panel-wrapper-index, 2);
-	}
+  .swipe-panel {
+    position: relative;
+    height: var(--sv-swipe-panel-height, 100%);
+    width: var(--sv-swipe-panel-width, inherit);
+  }
+  .swipe-item-wrapper {
+    overflow: hidden;
+    position: relative;
+    height: inherit;
+    z-index: var(--sv-swipe-panel-wrapper-index, 2);
+  }
 
-	.swipeable-total_elements,
-	.swipeable-slot-wrapper {
-		position: relative;
-		width: inherit;
-		height: inherit;
-	}
+  .swipeable-total_elements,
+  .swipeable-slot-wrapper {
+    position: relative;
+    width: inherit;
+    height: inherit;
+  }
 
-	.swipe-indicator {
-		position: relative;
-		bottom: 1.5rem;
-		display: flex;
-		justify-content: center;
-		z-index: var(--sv-swipe-panel-wrapper-index, 2);
-		pointer-events: none;
-	}
+  .swipe-indicator {
+    position: relative;
+    bottom: 1.5rem;
+    display: flex;
+    justify-content: center;
+    z-index: var(--sv-swipe-panel-wrapper-index, 2);
+    pointer-events: none;
+  }
 
-	.dot {
-		height: 10px;
-		width: 10px;
-		background-color: transparent;
-		border: 1px solid grey;
-		border-radius: 50%;
-		display: inline-block;
-		margin: 0px 2px;
-		cursor: pointer;
-		pointer-events: fill;
-	}
-	.swipe-indicator .is-active {
-		background-color: var(--sv-swipe-indicator-active-color, grey);
-	}
+  .dot {
+    height: 10px;
+    width: 10px;
+    background-color: transparent;
+    border: 1px solid grey;
+    border-radius: 50%;
+    display: inline-block;
+    margin: 0px 2px;
+    cursor: pointer;
+    pointer-events: fill;
+  }
+  .swipe-indicator .is-active {
+    background-color: var(--sv-swipe-indicator-active-color, grey);
+  }
 </style>
 
 <svelte:window on:touchstart={onMoveStart} on:mousedown={onMoveStart} />
 
 <div class="swipe-panel">
-	<div class="swipe-item-wrapper" bind:this={swipeWrapper}>
-		<div class="swipeable-total_elements">
-			<div class="swipeable-slot-wrapper">
-				<slot />
-			</div>
-		</div>
-	</div>
-	{#if showIndicators}
-		<div class="swipe-indicator swipe-indicator-inside">
-			{#each indicators as x, i}
-				<span
-					class="dot {activeIndicator == i ? 'is-active' : ''}"
-					on:click={() => {
-						changeItem(i);
-					}} />
-			{/each}
-		</div>
-	{/if}
+  <div class="swipe-item-wrapper" bind:this={swipeWrapper}>
+    <div class="swipeable-total_elements">
+      <div class="swipeable-slot-wrapper">
+        <slot />
+      </div>
+    </div>
+  </div>
+  {#if showIndicators}
+    <div class="swipe-indicator swipe-indicator-inside">
+      {#each indicators as x, i}
+        <span
+          class="dot {activeIndicator == i ? 'is-active' : ''}"
+          on:click={() => {
+            changeItem(i);
+          }} />
+      {/each}
+    </div>
+  {/if}
 </div>
