7df8f26cbfcf3fdd4da33cfbe2fd61a22f9ed604
Sharif Ahmed <me.sharifahmed@gmail.com>
2021-03-26 15:26:45 +0600

    simple swipe item

--- src/SwipeItem.svelte
+++ src/SwipeItem.svelte
@@ -1,60 +1,5 @@
 <script>
-    import { onMount, afterUpdate, createEventDispatcher} from 'svelte';
-    export let active = false;
-    export let classes = '';
-    export let style ='';
-    export let track_height = false;
-
-    let swipeItemInner = null;
-    let _height = 0;
-    const dispatch = createEventDispatcher();
-
-    console.log('swipeitem')
-
-    function pollForRender(){
-          console.log({active, track_height, _height})
-      if(swipeItemInner && typeof swipeItemInner != 'undefined'){
-        init();
-      }else{
-        setTimeout(() => {
-         pollForRender();
-        }, 200);
-      }
-    };
-
-    function fireSizeChange(){
-      console.log('fireSizeChange')
-        if(!swipeItemInner || swipeItemInner == 'undefined'){
-          pollForRender();
-        }else{
-          requestAnimationFrame(() => {
-            if(!swipeItemInner || typeof swipeItemInner == 'undefined'){
-              return;
-            }
-            var h1 = swipeItemInner.scrollHeight,
-            h2 = swipeItemInner.clientHeight;
-                  // console.log(swipeItemInner.scrollHeight)
-            dispatch('heightChange', {height: Math.max(h1, h2)});
-          });
-        }
-    }
-    function init(){
-        fireSizeChange();
-    }
-
-
-    afterUpdate(() => {
-      console.log({active, _height})
-      if(_height){
-        requestAnimationFrame(fireSizeChange)
-      }
-    });
-
-    onMount(() => {
-      setTimeout(() => {
-        pollForRender()
-      }, 100)
-    });
+  export let classes = '';
 </script>
 
 <style>
@@ -68,9 +13,6 @@
   }
 </style>
 
-
-<div bind:clientHeight={_height} class="swipeable-item {classes} {active ? 'is-active' : ''} " style="{style}">
-  <div bind:this={swipeItemInner} class="swipeable-item-inner">
-    <slot></slot>
-  </div>
+<div class="swipeable-item {classes}">
+    <slot />
 </div>
