6e11763200ce0b07500d6d6cffc88c6dab7b5493
SharifClick <me.sharifahmed@gmail.com>
2023-03-10 12:16:16 +0000

    Deploying to gh-pages from @ SharifClick/svelte-swipe@d2e4cbf446cdf744dd1f2f2f0367f3c8d09efed7 🚀

---
+++ _app/immutable/chunks/2-6a2e4dc3.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-da6a98c4.js";export{t as component};
--- _app/immutable/chunks/2-ee73e393.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-a570405d.js";export{t as component};
